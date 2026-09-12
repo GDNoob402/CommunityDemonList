7af8753cdd2d475308efd8970d8fc89d19e27540
GDNoob402 <gmd.noo8@gmail.com>
2026-09-12 13:34:30 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -78,7 +78,7 @@ export default {
                     </table>
                 </div>
                 <div v-else-if="!selected" class="level" style="height: 100%; display: flex; justify-content: center; align-items: center; text-align: center;">
-                    <h2>Welcome to the Community FPS Spam Challenge List!</h2>
+                    <h2>Welcome to the Community Demon List!</h2>
                     <p>This is a list where you can vote on placements of levels!</p>
                     <p>Join the discord to vote!</p>
                     <h2>le daily</h2>
